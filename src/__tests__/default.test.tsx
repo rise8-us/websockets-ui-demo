f52e507d10415a72be3715575ded887b2b91c775
David Lamberson <dlamberson@rise8.us>
2024-07-15 10:20:06 -0400

    test: add react testing library

--- src/__tests__/default.test.tsx
+++ src/__tests__/default.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
 describe('default framework test', () => { 
   test('should pass', () => {
     expect(true).toBe(true);
+  });
+
+  test('should handle rtl', () => {
+    render(<div>Test</div>);
+    expect(screen.getByText('Test')).toBeInTheDocument();
   });
  })
